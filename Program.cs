5719a423486d2d5efbe19db202f59556df87be8e
Arthur Engler <arthur_engler150@estudante.sesisenai.org.br>
2026-06-15 20:43:20 -0300

    q8

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
-﻿Console.Write("Informe o primeiro número: ");
-int numero1 = int.Parse(Console.ReadLine());
+﻿Console.Write("Informe a primeira pontuação: ");
+int pontuacao1 = int.Parse(Console.ReadLine());
 
-Console.Write("Informe o segundo número: ");
-int numero2 = int.Parse(Console.ReadLine());
+Console.Write("Informe a segunda pontuação: ");
+int pontuacao2 = int.Parse(Console.ReadLine());
 
-if (numero1 > numero2)
+Console.Write("Informe a terceira pontuação: ");
+int pontuacao3 = int.Parse(Console.ReadLine());
+
+if (pontuacao1 == pontuacao2 && pontuacao2 == pontuacao3)
 {
-    Console.WriteLine("O primeiro número é maior.");
+    Console.WriteLine("As três pontuações são iguais.");
 }
-else if (numero2 > numero1)
+else if (pontuacao1 >= pontuacao2 && pontuacao1 >= pontuacao3)
 {
-    Console.WriteLine("O segundo número é maior.");
+    Console.WriteLine("Maior pontuação: " + pontuacao1);
+}
+else if (pontuacao2 >= pontuacao1 && pontuacao2 >= pontuacao3)
+{
+    Console.WriteLine("Maior pontuação: " + pontuacao2);
 }
 else
 {
-    Console.WriteLine("Os números são iguais.");
+    Console.WriteLine("Maior pontuação: " + pontuacao3);
 }
